f4f546cf6669175c3954304b6f438c970c15d095
Leelsmuth <femianjorin@rocketmail.com>
2020-02-12 23:42:53 +0100

    completed the Player class

--- src/player.py
+++ src/player.py
@@ -6,3 +6,5 @@ class Player:
       # attributes (name, categories)
     self.name = name
     self.current_room = current_room
+  def __str__(self):
+    return f"{self.name} is currently in {self.current_room}"
